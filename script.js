be10cbbfa83bd035705a972358361e04aeb4071e
R3tr00o <pawebia921@student.polsl.pl>
2023-09-22 17:03:47 +0200

    Add player Selection and adapt typing check

--- script.js
+++ script.js
@@ -10,3 +10,22 @@ function getComputerChoice() {
     return "Scissors";
   }
 }
+
+function playerSelection(chosenItem) {
+  const lowerCase = chosenItem.toLowerCase();
+  const capitalizedStr = lowerCase.charAt(0).toUpperCase() + lowerCase.slice(1);
+
+  if (
+    capitalizedStr === "Paper" ||
+    capitalizedStr === "Rock" ||
+    capitalizedStr === "Scissors"
+  ) {
+    return capitalizedStr;
+  } else if (capitalizedStr) {
+    return alert("You typed something wrong. Refresh site :D");
+  } else {
+    return alert("You enter nothing, please try again.");
+  }
+}
+
+console.log(playerSelection(prompt("Choose: Paper, Rock or Scissors")));
